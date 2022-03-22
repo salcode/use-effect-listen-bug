ffdf121715ce454318dfada44a52bd78d6e61070
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-03-22 15:41:28 -0400

    Add components to App
    Add components ButtonWithListener and ColorPicker to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { useState } from 'react';
+import ButtonWithListener from './components/ButtonWithListener';
+import ColorPicker from './components/ColorPicker';
 
 function App() {
   const [color, setColor] = useState('red');
@@ -8,6 +10,15 @@ function App() {
       <header className="App-header">
         <h1>useEffect with Listener</h1>
       </header>
+      <ColorPicker
+        selectedColor={color}
+        setColor={setColor}
+      />
+      <ButtonWithListener
+        onEvent={
+          () => alert(`The color is ${color}`)
+        }
+      />
     </div>
   );
 }
